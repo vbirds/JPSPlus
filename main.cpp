34b006add5f2360fd8f57a61bbbffacf9a4309cb
vbirds <841952496@qq.com>
2022-02-26 10:14:07 +0800

    refactor: 删除不用代码

--- main.cpp
+++ main.cpp
@@ -20,60 +20,6 @@
 #include "Wrapper.h"
 #include "Log.h"
 
-void LoadMap(const char *fname, std::vector<bool> &map, int &w, int &h);
-
-struct stats {
-    std::vector<double> times;
-    std::vector<xyLoc> path;
-    std::vector<int> lengths;
-
-    double GetTotalTime() {
-        return std::accumulate(times.begin(), times.end(), 0.0);
-    }
-
-    double GetMaxTimestep() {
-        return *std::max_element(times.begin(), times.end());
-    }
-
-    double Get20MoveTime() {
-        for (unsigned int x = 0; x < lengths.size(); x++)
-            if (lengths[x] >= 20)
-                return std::accumulate(times.begin(), times.begin() + 1 + x, 0.0);
-        return GetTotalTime();
-    }
-
-    double GetPathLength() {
-        double len = 0;
-        for (int x = 0; x < (int) path.size() - 1; x++) {
-            if (path[x].x == path[x + 1].x || path[x].y == path[x + 1].y) {
-                len++;
-            } else {
-                len += 1.4142;
-            }
-        }
-        return len;
-    }
-
-    bool ValidatePath(int width, int height, const std::vector<bool> &mapData) {
-        for (int x = 0; x < (int) path.size() - 1; x++) {
-            if (abs(path[x].x - path[x + 1].x) > 1)
-                return false;
-            if (abs(path[x].y - path[x + 1].y) > 1)
-                return false;
-            if (!mapData[path[x].y * width + path[x].x])
-                return false;
-            if (!mapData[path[x + 1].y * width + path[x + 1].x])
-                return false;
-            if (path[x].x != path[x + 1].x && path[x].y != path[x + 1].y) {
-                if (!mapData[path[x + 1].y * width + path[x].x])
-                    return false;
-                if (!mapData[path[x].y * width + path[x + 1].x])
-                    return false;
-            }
-        }
-        return true;
-    }
-};
 
 int main(int argc, char *argv[]) {
     double allTestsTotalTime = 0;
